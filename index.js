e763fcc58e780fe78bae7fee87bd1b31d45d14eb
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-14 00:05:19 +0600

    jwt token

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({ error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+} 
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ec8hxwt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -81,11 +100,16 @@ async function run() {
 
     const selectedClassCollection = client.db('PhotoMe').collection('selectedClass');
 
-    app.get('/selectedClass', async (req, res) => {
+    app.get('/selectedClass', verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         return res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbidden access'})
       }
 
       const query = { email: email };
