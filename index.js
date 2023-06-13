e8aa644558f799f9c4df0669649a51160a9d46ee
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-13 18:48:16 +0600

    delete method

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config()
 
 
  
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ec8hxwt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -74,12 +74,12 @@ async function run() {
     })
 
 
-
-
-   
-
-
-
+    app.delete('/selectedClass/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
