182a8fc54a1c56d39250b77004b6b5ea764e7894
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-06-15 21:33:52 +0600

    payment api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -10,23 +12,21 @@ app.use(cors());
 app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
-
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error: true, message: 'unauthorized access'})
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
   }
 
   const token = authorization.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({ error: true, message: 'unauthorized access'})
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
     req.decoded = decoded;
     next();
-  })
-
-} 
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ec8hxwt.mongodb.net/?retryWrites=true&w=majority`;
@@ -42,16 +42,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const usersCollection = client.db('PhotoMe').collection('users');
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res.json({ token });
     });
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    };
 
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -69,6 +79,26 @@ async function run() {
 
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.get('/users/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+
+      try {
+        const user = await usersCollection.findOne(query);
+
+        if (!user) {
+          return res.status(404).send({ error: true, message: 'User not found' });
+        }
+
+        const isAdmin = user.role === 'admin';
+
+        res.send({ admin: isAdmin });
+      } catch (error) {
+        console.error('Error while checking admin status:', error);
+        res.status(500).send({ error: true, message: 'Internal server error' });
+      }
     });
 
     app.patch('/users/admin/:id', async (req, res) => {
@@ -98,6 +128,23 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/classes', async (req, res) => {
+      const newItem = req.body;
+
+      try {
+        const result = await classCollection.insertOne(newItem);
+        if (result.insertedCount > 0) {
+          const insertedClass = result.ops[0];
+          res.status(201).send(insertedClass);
+        } else {
+          res.status(500).send({ error: 'Failed to save the class' });
+        }
+      } catch (error) {
+        console.error('Error saving class:', error);
+        res.status(500).send({ error: 'An error occurred while saving the class' });
+      }
+    });
+
     const selectedClassCollection = client.db('PhotoMe').collection('selectedClass');
 
     app.get('/selectedClass', verifyJWT, async (req, res) => {
@@ -108,8 +155,8 @@ async function run() {
       }
 
       const decodedEmail = req.decoded.email;
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: 'forbidden access'})
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' });
       }
 
       const query = { email: email };
@@ -129,6 +176,71 @@ async function run() {
       const result = await selectedClassCollection.deleteOne(query);
       res.send(result);
     });
+
+    const paymentCollection = client.db('PhotoMe').collection('payments');
+
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post('/payments', verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error('Error creating payment intent:', error);
+        res.status(500).send({ error: 'An error occurred while creating the payment intent' });
+      }
+    });
+
+    app.post('/save-payment', async (req, res) => {
+      const payment = req.body;
+    
+      try {
+        const result = await paymentCollection.insertOne(payment);
+        res.send(result);
+      } catch (error) {
+        console.error('Error saving payment:', error);
+        res.status(500).send({ error: 'An error occurred while saving the payment' });
+      }
+    });
+
+    app.get('/save-payment', verifyJWT, async (req, res) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+    
+      try {
+        const payments = await paymentCollection.find(query).toArray();
+        res.send(payments);
+      } catch (error) {
+        console.error('Error retrieving payments:', error);
+        res.status(500).send({ error: 'An error occurred while retrieving the payments' });
+      }
+    });
+    
+    
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
